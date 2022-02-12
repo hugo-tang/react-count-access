7de925f2b9fa50a978275cd4ee5ac051160a4d73
tang.hugo1999 <tang.hugo1999@gmail.com>
2022-02-12 17:48:55 +0100

    jenkinsfile test

---
+++ frontend/src/__test__/App.test.js
@@ -0,0 +1,14 @@
+import { render, screen} from '@testing-library/react';
+import App from '../App';
+
+test('Sample test', () => {
+  expect(null).toBe(null);
+
+})
+
+test('Text : The current count is', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/The current count is/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
